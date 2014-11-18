7e2865bf5e13c12dd64038ed47cc584c417c3797
ozzerz <chaste@b10p12.fil.univ-lille1.fr>
2014-11-18 17:16:29 +0100

    ajout du modele pour le bucket

---
+++ src/model/Bucket.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+/**
+* design for a bucket
+*
+* @author Ozzerz
+*
+*/
+public class Bucket {
+	/**
+	 * the name of our bucket
+	 */
+	private String name;
+	/**
+	 * the list of all callStack 
+	 */
+	private ArrayList<CallStack> callStacks ;
+	
+	private Bucket(String name)
+	{
+		this.name=name;
+		this.callStacks=new ArrayList<CallStack>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<CallStack> getCallStacks() {
+		return callStacks;
+	}
+	public void setCallStacks(ArrayList<CallStack> callStacks) {
+		this.callStacks = callStacks;
+	}
+	/**
+	 * add a callStack to this bucket
+	 * @param callStack the callStack we want to add
+	 */
+	public void addCallStack(CallStack callStack)
+	{
+		this.callStacks.add(callStack);
+	}
+	
+	
+}
