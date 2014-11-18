8b7f0a1339b9b544bd7149778f0095861a3dc4e6
berenguer <berenguer@users.noreply.github.com>
2014-11-19 00:05:44 +0100

    appel test de Processor.similarity

--- src/code/Main.java
+++ src/code/Main.java
@@ -4,46 +4,44 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import classification.Processor;
 import model.Bucket;
 import model.CallStack;
 
 public class Main {
 
-	public static void main(String[] args) {
-		String directory="lib";
-		Parser p = new Parser();
-		CallStack callStack = p.read("lib/100004.json");
-		System.out.println("----------callStack--------------");
-		System.out.println(callStack.toString());
-		p.addGroupId(callStack, "lib/100004.json");
-		System.out.println("----------callStack--------------");
+    public static void main(String[] args) {
+        String directory = "lib";
+        Parser p = new Parser();
+        CallStack callStack = p.read("lib/100004.json");
+        System.out.println("----------callStack--------------");
+        System.out.println(callStack.toString());
+        p.addGroupId(callStack, "lib/100004.json");
+        System.out.println("----------callStack--------------");
 
-/*
-		//CREATION BUCKET
-		ArrayList<CallStack> allCallStack = p.readAll(directory);
-		//add all id
-		for (int i = 0; i < allCallStack.size(); i++) {
-			p.addGroupId(allCallStack.get(i), directory+"/"+allCallStack.get(i).getFilename());
-		}
-		System.out.println("--------------on  a tout les id");
-		Bucket bucket = new Bucket();
-		ArrayList<Bucket>allBuck=bucket.creationAllBucket(allCallStack);
-		System.out.println("-----------------on a tout les bucket");
-		File rep=new File("bucket");
-		rep.mkdir();
-		for (int i=0;i<allBuck.size();i++)
-		{
-			try {
+        CallStack c2 = p.read("lib/100007.json");
+        System.out.println("----------callStack--------------");
+        System.out.println(c2.toString());
+        p.addGroupId(c2, "lib/100007.json");
+        System.out.println("----------callStack--------------");
+        
+        Processor processor = new Processor();
+        processor.similarity(callStack, c2);
 
-				allBuck.get(i).createFile("bucket");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-	*/}
-
-
+        /*
+         * //CREATION BUCKET ArrayList<CallStack> allCallStack =
+         * p.readAll(directory); //add all id for (int i = 0; i <
+         * allCallStack.size(); i++) { p.addGroupId(allCallStack.get(i),
+         * directory+"/"+allCallStack.get(i).getFilename()); }
+         * System.out.println("--------------on  a tout les id"); Bucket bucket
+         * = new Bucket();
+         * ArrayList<Bucket>allBuck=bucket.creationAllBucket(allCallStack);
+         * System.out.println("-----------------on a tout les bucket"); File
+         * rep=new File("bucket"); rep.mkdir(); for (int
+         * i=0;i<allBuck.size();i++) { try {
+         * 
+         * allBuck.get(i).createFile("bucket"); } catch (IOException e) { //
+         * TODO Auto-generated catch block e.printStackTrace(); } }
+         */}
 
 }
