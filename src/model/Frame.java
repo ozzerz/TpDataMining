28264cf03dfb4b1d90ea193ccccc743ab83c38c4
berenguer <berenguer@users.noreply.github.com>
2014-11-21 09:14:59 +0100

    ajout equals

--- src/model/Frame.java
+++ src/model/Frame.java
@@ -7,41 +7,52 @@ package model;
  *
  */
 public class Frame {
-    /**
-     * the source ,example: TypeDeclaration.java:839
-     */
-    private String source;
-    /**
-     * the method who raise an error example :
-     * org.eclipse.jdt.internal.compiler.ast.TypeDeclaration.parseMethod
-     */
-    private String method;
+	/**
+	 * the source ,example: TypeDeclaration.java:839
+	 */
+	private String source;
+	/**
+	 * the method who raise an error example :
+	 * org.eclipse.jdt.internal.compiler.ast.TypeDeclaration.parseMethod
+	 */
+	private String method;
 
-    public Frame(String source, String method) {
-        super();
-        this.source = source;
-        this.method = method;
-    }
+	public Frame(String source, String method) {
+		super();
+		this.source = source;
+		this.method = method;
+	}
 
-    public String getSource() {
-        return source;
-    }
+	public String getSource() {
+		return source;
+	}
 
-    public void setSource(String source) {
-        this.source = source;
-    }
+	public void setSource(String source) {
+		this.source = source;
+	}
 
-    public String getMethod() {
-        return method;
-    }
+	public String getMethod() {
+		return method;
+	}
 
-    public void setMethod(String method) {
-        this.method = method;
-    }
+	public void setMethod(String method) {
+		this.method = method;
+	}
 
-    public String toString() {
-        return "source: " + source + " method: " + method;
+	public String toString() {
+		return "source: " + source + " method: " + method;
 
-    }
+	}
+	
+	@Override
+	public boolean equals(Object frame) {
+	    if (frame == null) return false;
+	    if (frame == this) return true;
+	    if (!(frame instanceof Frame)) return false;
+	    
+	    Frame frameClass = (Frame) frame;
+	    return (this.method.equals(frameClass.method) &&
+	            this.source.equals(frameClass.source));
+	}
 
 }
