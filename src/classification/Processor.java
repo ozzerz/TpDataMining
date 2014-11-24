a846de9069cfd75aa3d70d1f6781e8c927b887ba
Pierre-Philippe <pp.berenguer@hotmail.fr>
2014-11-24 17:48:29 +0100

    stacks et marix variable de class, update cost

--- src/classification/Processor.java
+++ src/classification/Processor.java
@@ -1,6 +1,7 @@
 package classification;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import model.CallStack;
 import model.Frame;
@@ -25,6 +26,12 @@ public class Processor {
      * "o"
      */
     private int offset;
+    
+    private double matrix[][];
+    
+    private CallStack c1;
+    
+    private CallStack c2;
 
     public Processor() {
         // empty
@@ -42,35 +49,30 @@ public class Processor {
     }
 
     public double similarity(CallStack c1, CallStack c2) {
-        double[][] matrix = new double[c1.getCallStack().size()][c2.getCallStack().size()];
+        this.c1 = c1;
+        this.c2 = c2;
+        this.matrix = new double[c1.getCallStack().size()][c2.getCallStack().size()];
         
         // nombre de frames de c1 et c2 identiques (appelee "l" dans l'article Windows
         int identic_frames = 0;
         
         // parcours de c1
-        for (int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < this.matrix.length; i++) {
             
             // parcours de c2
-            for (int j = 0; j < matrix[i].length; j++) {
+            for (int j = 0; j < this.matrix[i].length; j++) {
                 
-                double previous_ij, previous_i, previous_j, cost;
-                previous_ij = previous_i = previous_j = cost = 0.0;
+                double previous_ij_cost, previous_i, previous_j, cost;
+                previous_ij_cost = previous_i = previous_j = cost = 0.0;
                 
-                // deux frames auront une similarite de 0 au minimun 
-                matrix[i][j] = cost;
-                
-                // si les deux frames sont identiques le poids est calcule
-                if (c1.getCallStack().get(i).equals(c2.getCallStack().get(j))) {
-                    matrix[i][j] = cost(i, j);
-                    identic_frames ++;
-                }
-                
+                cost = cost(i, j);
+
                 if ((i > 0) && (j > 0)) {
-                    previous_ij = matrix[i-1][j-1] + cost;
+                    previous_ij_cost = this.matrix[i-1][j-1] + cost;
                 } else if (i > 0) {
-                    previous_i = matrix[i-1][j];
+                    previous_i = this.matrix[i-1][j];
                 } else if (j > 0) {
-                    previous_i = matrix[i][j-1];
+                    previous_i = this.matrix[i][j-1];
                 }
                 
                 /*
@@ -81,14 +83,15 @@ public class Processor {
                  * ou matrix[i][j-1]
                  */
                 double max_value = Math.max(cost,
-                        Math.max(previous_ij + cost,
+                        Math.max(previous_ij_cost,
                                 Math.max(previous_i, previous_j)));
                 
                 // enregistrement dans la matrice
-                matrix[i][j] = max_value;
+                //System.out.println(max_value);
+                this.matrix[i][j] = max_value;
                 
             } // fin j parcours de c2
-
+            System.out.println(Arrays.toString(this.matrix[i]));
         } // fin i parcours de c1
         
         // similarite vaut 0 si aucune frames en commun (a verifier!) ==> evite d'avoir une similarite "Infinity" qui pourrait mal interprete
@@ -99,7 +102,7 @@ public class Processor {
             sum = sum + Math.exp(-this.coefficient * l);
         }        
         // sim (4) dans l'article Windows
-        return (c1.getCallStack().size() * c2.getCallStack().size()) / sum;
+        return this.matrix[this.matrix.length-1][this.matrix[this.matrix.length-1].length-1];
     }
 
 
@@ -115,12 +118,17 @@ public class Processor {
      */
     protected double cost(int i, int j) {
         //top frame
-        double top = Math.exp(-this.coefficient * Math.min(i, j));
+        if (this.c1.getCallStack().get(i) == this.c2.getCallStack().get(j)) {
+            double top = Math.exp(-this.coefficient * Math.min(i, j));
+            System.out.println("top : "+top);
+            // alignement offset
+            double offset = Math.exp(-this.offset * Math.abs(i-j));
+            System.out.println("offset : "+offset);
+            
+            return top * offset;
+        }
         
-        // alignement offset
-        double offset = Math.exp(-this.offset * Math.abs(i-j));
-
-        return top * offset;
+        return 0.0;
 
     }
 
