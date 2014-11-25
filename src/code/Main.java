655781fca9a6ef205b18ce8fe0a24ad433f35e78
Pierre-Philippe <pp.berenguer@hotmail.fr>
2014-11-25 17:57:34 +0100

    the commit du Main yeah

--- src/code/Main.java
+++ src/code/Main.java
@@ -3,18 +3,25 @@ package code;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import classification.Processor;
 import model.Bucket;
 import model.CallStack;
 
 public class Main {
+    
 
     public static void main(String[] args) {
         String directory = "lib";
         Parser p = new Parser();
         CallStack callStack = p.read("lib/206001.json");
         CallStack c2 = p.read("lib/206001.json");
+        
+        ArrayList<Bucket> oneFileOneBucket = p.oneFileOneBucket("lib");
+        
+        Processor processor = new Processor();
+        processor.bucketize(oneFileOneBucket, 0.4);
         
         /* MAX TES SYSOUT PRENNENT TROP DE PLACE
         System.out.println("----------callStack--------------");
@@ -25,21 +32,39 @@ public class Main {
         //p.addGroupId(c2, "lib/206001.json"); <==Même question
          */
         
+        /*
         CallStack c3 = p.read("lib/49499.json");
         CallStack c4 = p.read("lib/48643.json");
         CallStack c5 = p.read("lib/49608.json");
+        CallStack c6 = p.read("lib/51358.json");
+        CallStack c7 = p.read("lib/49406.json");
         
         Processor processor = new Processor();
         System.out.println("----------- Similarity ------------");
         System.out.println("-------- callstacks identiques :");
         System.out.println(processor.similarity(callStack, c2));
+        
+        System.out.println("-------- callstacks identiques :");
+        System.out.println(processor.similarity(c2, c2));
+        
+        System.out.println("-------- callstacks identiques :");
+        System.out.println(processor.similarity(c3, c3));
+        
+        System.out.println("-------- callstacks identiques :");
+        System.out.println(processor.similarity(c6, c6));
+        
         System.out.println("-------- callstacks differentes :");
         System.out.println(processor.similarity(callStack, c4));
+        
         System.out.println("-------- callstacks du meme bucket :");
         System.out.println(processor.similarity(c3, c4));
+        
         System.out.println("-------- callstacks du meme bucket :");
         System.out.println(processor.similarity(c3, c5));
-
+        
+        System.out.println("-------- callstacks du meme bucket :");
+        System.out.println(processor.similarity(c6, c7));
+        */
         
         /*
         //CREATION BUCKET 
