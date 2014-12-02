b39b390c0f307c6c0245f7bf37715b87ce89d9c4
berenguer <berenguer@users.noreply.github.com>
2014-12-02 03:11:05 +0100

    main inactif decommenter avec exec

--- src/code/Main.java
+++ src/code/Main.java
@@ -17,38 +17,30 @@ import model.CallStack;
 
 public class Main {
 
-    public void createBuckets(String inputDir, String outputDir) {
-        String directory="Votre repertoire";
-        String resultDirectory="Le repertoire contenant vaut résultat";
+    public static void createBuckets(String inputDir, String outputDir) {
+
         Parser p = new Parser();
         ArrayList<CallStack> allCallStack = p.readAll(inputDir); // add all id
         for (int i = 0; i < allCallStack.size(); i++) {
 
-                p.addGroupIdAndDuplicateId(allCallStack.get(i), directory + "/"
-                                + allCallStack.get(i).getFilename());
+            p.addGroupIdAndDuplicateId(allCallStack.get(i), inputDir + "/"
+                    + allCallStack.get(i).getFilename());
         }
         Bucket bucket = new Bucket();
         ArrayList<Bucket> allBuck = bucket.creationAllBucket(allCallStack);
-        
-        File rep = new File(outputDir);//création du répértoire
+
+        File rep = new File(outputDir);// création du répértoire
         rep.mkdir();
         for (int i = 0; i < allBuck.size(); i++) {
-                try {
-                        allBuck.get(i).createFile(outputDir);
-                } catch (IOException e) { //
-                }
+            try {
+                allBuck.get(i).createFile(outputDir);
+            } catch (IOException e) { //
+            }
         }
 
     }
-    
 
-    /**
-     * Recuperer seulement les 800 buckets les plus volumineux (les autres n'ont qu'un fichier de rapport)
-     * @param repFin
-     * @throws FileNotFoundException
-     */
-    public static void recup800Bucket(String repFin)
-            throws FileNotFoundException {
+    public static void recup800Bucket(String repFin) throws IOException {
         // on créer le repertoire
         File rep = new File(repFin);// création du répértoire
         if (!rep.exists()) {
@@ -82,24 +74,38 @@ public class Main {
             e.printStackTrace();
         }
 
-        // pour chacun des fichier gardé on va copier le fichier dans le
-        // repertoire de destination
-        for (int fic = 0; fic < fichierPile.size(); fic++) {
-            System.out.println("on essaie de copier le fichier");
-            String newFileName = repFin
-                    + "/"
-                    + (fichierPile.get(fic).substring(fichierPile.get(fic)
-                            .indexOf("/") + 1));
-            System.out.println(newFileName);
-            copyFile(new File(fichierPile.get(fic)), new File(newFileName));
+        EnregistrePile(fichierPile, repFin);
+
+    }
+
+    public static void EnregistrePile(ArrayList<String> buckets, String repFin)
+            throws IOException {
+        for (int i = 0; i < buckets.size(); i++) {
+            File fichier = new File(buckets.get(i).trim());
+            System.out.println(fichier.toPath());
+            if (fichier.exists()) {
+                
+                System.out.println("test 2 " + buckets.get(i).trim());
+                InputStream ips = new FileInputStream(fichier);
+                InputStreamReader ipsr = new InputStreamReader(ips);
+                BufferedReader br = new BufferedReader(ipsr);
+                String ligne = br.readLine();
+                while (ligne != null) {
+                    System.out.println(ligne);
+                    System.out.println("test  " + "lib/" + ligne);
+                    String newFileName = repFin + "/" + ligne;
+                    if (new File("lib/" + ligne).exists()) {
+                        copyFile(new File("lib/" + ligne),
+                                new File(newFileName));
+                    }
+                    ligne = br.readLine();
+
+                }
+            }
         }
 
     }
 
-    /**
-     * copie le fichier source dans le fichier resultat retourne vrai si cela
-     * réussit
-     */
     public static boolean copyFile(File source, File dest) {
         try {
             // Declaration et ouverture des flux
@@ -133,27 +139,21 @@ public class Main {
         return true; // Résultat OK
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        
-   
+    public static void main(String[] args) throws IOException {
+        //createBuckets("800/", "800bucketed");
+        //recup800Bucket("800buketed");
 
-        Bucket buck = new Bucket();
-        Processor processor = new Processor();
-        String directory = "mini-lib2";
-        String directoryResultat = "results";
-        double minSimilarity = 0.6;
-        Parser p = new Parser();
-        ArrayList<Bucket> buckets = p.oneFileOneBucket(directory);
-        buckets = processor.bucketize(buckets, minSimilarity);
-        File rep = new File(directoryResultat);// creation du repertoire
-        rep.mkdir();
-        for (int i = 0; i < buckets.size(); i++) {
-            try {
-                buckets.get(i).createFile(directoryResultat,
-                        buckets.get(i).getCallStacks().get(0).getFilename());
-            } catch (IOException e) {
-            }
-        }
-
+        /*
+         * Bucket buck = new Bucket(); Processor processor = new Processor();
+         * String directory = "mini-lib2"; String directoryResultat = "results";
+         * double minSimilarity = 0.6; Parser p = new Parser();
+         * ArrayList<Bucket> buckets = p.oneFileOneBucket(directory); buckets =
+         * processor.bucketize(buckets, minSimilarity); File rep = new
+         * File(directoryResultat);// creation du repertoire rep.mkdir(); for
+         * (int i = 0; i < buckets.size(); i++) { try {
+         * buckets.get(i).createFile(directoryResultat,
+         * buckets.get(i).getCallStacks().get(0).getFilename()); } catch
+         * (IOException e) { } }
+         */
     }
 }
